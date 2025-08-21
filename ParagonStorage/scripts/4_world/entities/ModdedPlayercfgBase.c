73103624ab1f51db87f76055c3c5c8d789815454
Fabricio <fabriciodaiane05@gmail.com>
2025-08-21 12:03:31 -0300

    Update Paragon_Storage

--- ParagonStorage/scripts/4_world/entities/ModdedPlayercfgBase.c
+++ ParagonStorage/scripts/4_world/entities/ModdedPlayercfgBase.c
@@ -4,6 +4,7 @@ modded class ModItemRegisterCallbacks
 	{
 		super.RegisterHeavy(pType, pBehavior);
 		pType.AddItemInHandsProfileIK("StorageBox_ColorBase", "dz/anims/workspaces/player/player_main/player_main_heavy.asi", pBehavior,			 		"dz/anims/anm/player/ik/heavy/wooden_crate.anm");
+		pType.AddItemInHandsProfileIK("RFSolarPanelKit", "dz/anims/workspaces/player/player_main/player_main_heavy.asi", pBehavior,			 		"dz/anims/anm/player/ik/heavy/wooden_crate.anm");
 		pType.AddItemInHandsProfileIK("WaterBarrel_ColorBase", "dz/anims/workspaces/player/player_main/player_main_heavy.asi", pBehavior,			 		"dz/anims/anm/player/ik/heavy/generator.anm");   			
 	};	
 };
