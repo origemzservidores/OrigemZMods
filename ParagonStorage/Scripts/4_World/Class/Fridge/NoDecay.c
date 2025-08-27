de9c47bcc6fe7098e532bb7f979f934fb6859d6a
Fabricio3028 <fabriciodaiane05@gmail.com>
2025-08-27 11:38:00 -0300

    Update ParagonStorage (Fridge)
    Adicionado nova forma de manter os alimentos fresco, Porem foi deixado mesmo sem a geladeira estiver ligada para teste.

---
+++ ParagonStorage/Scripts/4_World/Class/Fridge/NoDecay.c
@@ -0,0 +1,57 @@
+// modded class Edible_Base extends ItemBase
+// {
+//     override void ProcessDecay(float delta, bool hasRootAsPlayer)
+//     {
+//         EntityAI parent = GetHierarchyParent();
+
+//         Fridge_ColorBase fridge;
+//         if (CastTo(fridge, parent) && fridge.IsWorkingNow())
+//         {
+//             return;
+//         }
+
+//         IceBox_ColorBase icebox;
+//         if (CastTo(icebox, parent) && icebox.IsWorkingNow())
+//         {
+//             return;
+//         }
+
+//         IceCeramFreezer_ColorBase iceceram;
+//         if (CastTo(iceceram, parent) && iceceram.IsWorkingNow())
+//         {
+//             return;
+//         }
+
+//         super.ProcessDecay(delta, hasRootAsPlayer);
+//     }
+// }
+
+//---------- NÃO APODRECE MESMO DESLIGADO ----------//
+
+modded class Edible_Base extends ItemBase
+{
+    override void ProcessDecay(float delta, bool hasRootAsPlayer)
+    {
+        EntityAI parent = GetHierarchyParent();
+
+        Fridge_ColorBase fridge;
+        if (CastTo(fridge, parent))
+        {
+            return;
+        }
+
+        IceBox_ColorBase icebox;
+        if (CastTo(icebox, parent))
+        {
+            return;
+        }
+
+        IceCeramFreezer_ColorBase iceceram;
+        if (CastTo(iceceram, parent))
+        {
+            return;
+        }
+
+        super.ProcessDecay(delta, hasRootAsPlayer);
+    }
+}
--- ParagonStorage/scripts/4_world/Class/Fridge/FridgeFoodNoDecay.c
+++ ParagonStorage/scripts/4_world/Class/Fridge/FridgeFoodNoDecay.c
@@ -1,426 +1,424 @@
-//NÃO DEIXA COMIDA ESTRAGAR
-modded class PigSteakMeat extends Edible_Base
-{
-	override bool CanDecay()
-	{
-		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
-		Fridge_ColorBase Fridge;
-		if (CastTo(Fridge, parent))
-		{
-			if(Fridge && Fridge.IsWorkingNow())
-			{
-			return false;	
-			}			
-		}
-		return super.CanDecay();
-	}
-}
+// //NÃO DEIXA COMIDA ESTRAGAR
+// modded class PigSteakMeat extends Edible_Base
+// {
+// 	override bool CanDecay()
+// 	{
+// 		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
+// 		Fridge_ColorBase Fridge;
+// 		if (CastTo(Fridge, parent))
+// 		{
+// 			if(Fridge && Fridge.IsWorkingNow())
+// 			{
+// 			return false;	
+// 			}			
+// 		}
+// 		return super.CanDecay();
+// 	}
+// }
 
-modded class Lard extends Edible_Base
-{
+// modded class Lard extends Edible_Base
+// {
 	
-	override bool CanDecay()
-	{
-		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
-		Fridge_ColorBase Fridge;
-		if (CastTo(Fridge, parent))
-		{
-			if(Fridge && Fridge.IsWorkingNow())
-			{
-			return false;	
-			}			
-		}
+// 	override bool CanDecay()
+// 	{
+// 		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
+// 		Fridge_ColorBase Fridge;
+// 		if (CastTo(Fridge, parent))
+// 		{
+// 			if(Fridge && Fridge.IsWorkingNow())
+// 			{
+// 			return false;	
+// 			}			
+// 		}
 	
-		return super.CanDecay();
-	}
+// 		return super.CanDecay();
+// 	}
 	
-}
+// }
 
-modded class Carp extends Edible_Base
-{
+// modded class Carp extends Edible_Base
+// {
 	
-	override bool CanDecay()
-	{
-		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
-		Fridge_ColorBase Fridge;
-		if (CastTo(Fridge, parent))
-		{
-			if(Fridge && Fridge.IsWorkingNow())
-			{
-			return false;	
-			}			
-		}
+// 	override bool CanDecay()
+// 	{
+// 		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
+// 		Fridge_ColorBase Fridge;
+// 		if (CastTo(Fridge, parent))
+// 		{
+// 			if(Fridge && Fridge.IsWorkingNow())
+// 			{
+// 			return false;	
+// 			}			
+// 		}
 	
 		
-		return super.CanDecay();
-	}
+// 		return super.CanDecay();
+// 	}
 	
 	
-}
+// }
 
-modded class CarpFilletMeat extends Edible_Base
-{
-	override bool CanDecay()
-	{
-		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
-		Fridge_ColorBase Fridge;
-		if (CastTo(Fridge, parent))
-		{
-			if(Fridge && Fridge.IsWorkingNow())
-			{
-			return false;	
-			}			
-		}
+// modded class CarpFilletMeat extends Edible_Base
+// {
+// 	override bool CanDecay()
+// 	{
+// 		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
+// 		Fridge_ColorBase Fridge;
+// 		if (CastTo(Fridge, parent))
+// 		{
+// 			if(Fridge && Fridge.IsWorkingNow())
+// 			{
+// 			return false;	
+// 			}			
+// 		}
 	
-		return super.CanDecay();
-	}
-}
+// 		return super.CanDecay();
+// 	}
+// }
 
-modded class MackerelFilletMeat extends Edible_Base
-{
+// modded class MackerelFilletMeat extends Edible_Base
+// {
 	
-	override bool CanDecay()
-	{
-		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
-		Fridge_ColorBase Fridge;
-		if (CastTo(Fridge, parent))
-		{
-			if(Fridge && Fridge.IsWorkingNow())
-			{
-			return false;	
-			}			
-		}
+// 	override bool CanDecay()
+// 	{
+// 		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
+// 		Fridge_ColorBase Fridge;
+// 		if (CastTo(Fridge, parent))
+// 		{
+// 			if(Fridge && Fridge.IsWorkingNow())
+// 			{
+// 			return false;	
+// 			}			
+// 		}
 	
 		
-		return super.CanDecay();
-	}
+// 		return super.CanDecay();
+// 	}
 	
 	
-}
+// }
 
-modded class Sardines extends Edible_Base
-{
+// modded class Sardines extends Edible_Base
+// {
 	
-	override bool CanDecay()
-	{
-		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
-		Fridge_ColorBase Fridge;
-		if (CastTo(Fridge, parent))
-		{
-			if(Fridge && Fridge.IsWorkingNow())
-			{
-			return false;	
-			}			
-		}
+// 	override bool CanDecay()
+// 	{
+// 		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
+// 		Fridge_ColorBase Fridge;
+// 		if (CastTo(Fridge, parent))
+// 		{
+// 			if(Fridge && Fridge.IsWorkingNow())
+// 			{
+// 			return false;	
+// 			}			
+// 		}
 	
 		
-		return super.CanDecay();
-	}
+// 		return super.CanDecay();
+// 	}
 	
 	
-}
+// }
 
-modded class Mackerel extends Edible_Base
-{
+// modded class Mackerel extends Edible_Base
+// {
 	
-	override bool CanDecay()
-	{
-		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
-		Fridge_ColorBase Fridge;
-		if (CastTo(Fridge, parent))
-		{
-			if(Fridge && Fridge.IsWorkingNow())
-			{
-			return false;	
-			}			
-		}
+// 	override bool CanDecay()
+// 	{
+// 		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
+// 		Fridge_ColorBase Fridge;
+// 		if (CastTo(Fridge, parent))
+// 		{
+// 			if(Fridge && Fridge.IsWorkingNow())
+// 			{
+// 			return false;	
+// 			}			
+// 		}
 	
 		
-		return super.CanDecay();
-	}
+// 		return super.CanDecay();
+// 	}
 	
 	
-}
+// }
 
-modded class ChickenBreastMeat extends Edible_Base
-{
+// modded class ChickenBreastMeat extends Edible_Base
+// {
 	
-	override bool CanDecay()
-	{
-		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
-		Fridge_ColorBase Fridge;
-		if (CastTo(Fridge, parent))
-		{
-			if(Fridge && Fridge.IsWorkingNow())
-			{
-			return false;	
-			}			
-		}
+// 	override bool CanDecay()
+// 	{
+// 		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
+// 		Fridge_ColorBase Fridge;
+// 		if (CastTo(Fridge, parent))
+// 		{
+// 			if(Fridge && Fridge.IsWorkingNow())
+// 			{
+// 			return false;	
+// 			}			
+// 		}
 	
 		
-		return super.CanDecay();
-	}
+// 		return super.CanDecay();
+// 	}
 	
 	
-}
+// }
 
-modded class GoatSteakMeat extends Edible_Base
-{
+// modded class GoatSteakMeat extends Edible_Base
+// {
 	
-	override bool CanDecay()
-	{
-		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
-		Fridge_ColorBase Fridge;
-		if (CastTo(Fridge, parent))
-		{
-			if(Fridge && Fridge.IsWorkingNow())
-			{
-			return false;	
-			}			
-		}
+// 	override bool CanDecay()
+// 	{
+// 		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
+// 		Fridge_ColorBase Fridge;
+// 		if (CastTo(Fridge, parent))
+// 		{
+// 			if(Fridge && Fridge.IsWorkingNow())
+// 			{
+// 			return false;	
+// 			}			
+// 		}
 	
 		
-		return super.CanDecay();
-	}
+// 		return super.CanDecay();
+// 	}
 	
 	
-}
+// }
 
-modded class SheepSteakMeat extends Edible_Base
-{
+// modded class SheepSteakMeat extends Edible_Base
+// {
 	
-	override bool CanDecay()
-	{
-		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
-		Fridge_ColorBase Fridge;
-		if (CastTo(Fridge, parent))
-		{
-			if(Fridge && Fridge.IsWorkingNow())
-			{
-			return false;	
-			}			
-		}
+// 	override bool CanDecay()
+// 	{
+// 		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
+// 		Fridge_ColorBase Fridge;
+// 		if (CastTo(Fridge, parent))
+// 		{
+// 			if(Fridge && Fridge.IsWorkingNow())
+// 			{
+// 			return false;	
+// 			}			
+// 		}
 	
 		
-		return super.CanDecay();
-	}
+// 		return super.CanDecay();
+// 	}
 	
 	
-}
+// }
 
-modded class BoarSteakMeat extends Edible_Base
-{
+// modded class BoarSteakMeat extends Edible_Base
+// {
 	
-	override bool CanDecay()
-	{
-		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
-		Fridge_ColorBase Fridge;
-		if (CastTo(Fridge, parent))
-		{
-			if(Fridge && Fridge.IsWorkingNow())
-			{
-			return false;	
-			}			
-		}
+// 	override bool CanDecay()
+// 	{
+// 		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
+// 		Fridge_ColorBase Fridge;
+// 		if (CastTo(Fridge, parent))
+// 		{
+// 			if(Fridge && Fridge.IsWorkingNow())
+// 			{
+// 			return false;	
+// 			}			
+// 		}
 	
 		
-		return super.CanDecay();
-	}
+// 		return super.CanDecay();
+// 	}
 	
 	
-}
+// }
 
-modded class WolfSteakMeat extends Edible_Base
-{
+// modded class WolfSteakMeat extends Edible_Base
+// {
 	
-	override bool CanDecay()
-	{
-		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
-		Fridge_ColorBase Fridge;
-		if (CastTo(Fridge, parent))
-		{
-			if(Fridge && Fridge.IsWorkingNow())
-			{
-			return false;	
-			}			
-		}
+// 	override bool CanDecay()
+// 	{
+// 		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
+// 		Fridge_ColorBase Fridge;
+// 		if (CastTo(Fridge, parent))
+// 		{
+// 			if(Fridge && Fridge.IsWorkingNow())
+// 			{
+// 			return false;	
+// 			}			
+// 		}
 	
 		
-		return super.CanDecay();
-	}
+// 		return super.CanDecay();
+// 	}
 	
 	
-}
+// }
 
-modded class DeerSteakMeat extends Edible_Base
-{
+// modded class DeerSteakMeat extends Edible_Base
+// {
 	
-	override bool CanDecay()
-	{
-		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
-		Fridge_ColorBase Fridge;
-		if (CastTo(Fridge, parent))
-		{
-			if(Fridge && Fridge.IsWorkingNow())
-			{
-			return false;	
-			}			
-		}
+// 	override bool CanDecay()
+// 	{
+// 		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
+// 		Fridge_ColorBase Fridge;
+// 		if (CastTo(Fridge, parent))
+// 		{
+// 			if(Fridge && Fridge.IsWorkingNow())
+// 			{
+// 			return false;	
+// 			}			
+// 		}
 	
 		
-		return super.CanDecay();
-	}
+// 		return super.CanDecay();
+// 	}
 	
 	
-}
+// }
 
-modded class CowSteakMeat extends Edible_Base
-{
+// modded class CowSteakMeat extends Edible_Base
+// {
 	
-	override bool CanDecay()
-	{
-		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
-		Fridge_ColorBase Fridge;
-		if (CastTo(Fridge, parent))
-		{
-			if(Fridge && Fridge.IsWorkingNow())
-			{
-			return false;	
-			}			
-		}
+// 	override bool CanDecay()
+// 	{
+// 		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
+// 		Fridge_ColorBase Fridge;
+// 		if (CastTo(Fridge, parent))
+// 		{
+// 			if(Fridge && Fridge.IsWorkingNow())
+// 			{
+// 			return false;	
+// 			}			
+// 		}
 	
 		
-		return super.CanDecay();
-	}
+// 		return super.CanDecay();
+// 	}
 	
 	
-}
+// }
 
-modded class BearSteakMeat extends Edible_Base
-{
+// modded class BearSteakMeat extends Edible_Base
+// {
 	
-	override bool CanDecay()
-	{
-		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
-		Fridge_ColorBase Fridge;
-		if (CastTo(Fridge, parent))
-		{
-			if(Fridge && Fridge.IsWorkingNow())
-			{
-			return false;	
-			}			
-		}
+// 	override bool CanDecay()
+// 	{
+// 		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
+// 		Fridge_ColorBase Fridge;
+// 		if (CastTo(Fridge, parent))
+// 		{
+// 			if(Fridge && Fridge.IsWorkingNow())
+// 			{
+// 			return false;	
+// 			}			
+// 		}
 	
 		
-		return super.CanDecay();
-	}
+// 		return super.CanDecay();
+// 	}
 	
 	
-}
+// }
 
-modded class Tomato : Edible_Base
-{
+// modded class Tomato : Edible_Base
+// {
 	
-	override bool CanDecay()
-	{
-		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
-		Fridge_ColorBase Fridge;
-		if (CastTo(Fridge, parent))
-		{
-			if(Fridge && Fridge.IsWorkingNow())
-			{
-			return false;	
-			}			
-		}
+// 	override bool CanDecay()
+// 	{
+// 		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
+// 		Fridge_ColorBase Fridge;
+// 		if (CastTo(Fridge, parent))
+// 		{
+// 			if(Fridge && Fridge.IsWorkingNow())
+// 			{
+// 			return false;	
+// 			}			
+// 		}
 	
 		
-		return super.CanDecay();
-	}
+// 		return super.CanDecay();
+// 	}
 	
 	
-}
+// }
 
-modded class Potato : Edible_Base
-{
+// modded class Potato : Edible_Base
+// {
 	
-	override bool CanDecay()
-	{
-		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
-		Fridge_ColorBase Fridge;
-		if (CastTo(Fridge, parent))
-		{
-			if(Fridge && Fridge.IsWorkingNow())
-			{
-			return false;	
-			}			
-		}
+// 	override bool CanDecay()
+// 	{
+// 		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
+// 		Fridge_ColorBase Fridge;
+// 		if (CastTo(Fridge, parent))
+// 		{
+// 			if(Fridge && Fridge.IsWorkingNow())
+// 			{
+// 			return false;	
+// 			}			
+// 		}
 	
-		return super.CanDecay();
-	}
+// 		return super.CanDecay();
+// 	}
 	
 	
-}
+// }
 
-modded class Zucchini : Edible_Base
-{
+// modded class Zucchini : Edible_Base
+// {
 	
-	override bool CanDecay()
-	{
-		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
-		Fridge_ColorBase Fridge;
-		if (CastTo(Fridge, parent))
-		{
-			if(Fridge && Fridge.IsWorkingNow())
-			{
-			return false;	
-			}			
-		}
+// 	override bool CanDecay()
+// 	{
+// 		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
+// 		Fridge_ColorBase Fridge;
+// 		if (CastTo(Fridge, parent))
+// 		{
+// 			if(Fridge && Fridge.IsWorkingNow())
+// 			{
+// 			return false;	
+// 			}			
+// 		}
 	
 		
-		return super.CanDecay();
-	}
+// 		return super.CanDecay();
+// 	}
 	
 	
-}
+// }
 
-modded class Pumpkin : Edible_Base
-{
+// modded class Pumpkin : Edible_Base
+// {
 	
-	override bool CanDecay()
-	{
-		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
-		Fridge_ColorBase Fridge;
-		if (CastTo(Fridge, parent))
-		{
-			if(Fridge && Fridge.IsWorkingNow())
-			{
-			return false;	
-			}			
-		}
+// 	override bool CanDecay()
+// 	{
+// 		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
+// 		Fridge_ColorBase Fridge;
+// 		if (CastTo(Fridge, parent))
+// 		{
+// 			if(Fridge && Fridge.IsWorkingNow())
+// 			{
+// 			return false;	
+// 			}			
+// 		}
 	
 		
-		return super.CanDecay();
-	}
+// 		return super.CanDecay();
+// 	}
 	
 	
-}
+// }
 
-modded class MushroomBase : Edible_Base
-{
+// modded class MushroomBase : Edible_Base
+// {
 	
-	override bool CanDecay()
-	{
-		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
-		Fridge_ColorBase Fridge;
-		if (CastTo(Fridge, parent))
-		{
-			if(Fridge && Fridge.IsWorkingNow())
-			{
-			return false;	
-			}			
-		}
-	
-		
-		return super.CanDecay();
-	}
-}
+// 	override bool CanDecay()
+// 	{
+// 		ItemBase parent = ItemBase.Cast( GetHierarchyParent());
+// 		Fridge_ColorBase Fridge;
+// 		if (CastTo(Fridge, parent))
+// 		{
+// 			if(Fridge && Fridge.IsWorkingNow())
+// 			{
+// 			return false;	
+// 			}			
+// 		}
+// 		return super.CanDecay();
+// 	}
+// }
